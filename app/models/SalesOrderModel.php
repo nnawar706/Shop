699bda9622f527b71b0686d7f80534e7217ec090
nnawar706 <nnawar706@gmail.com>
2023-02-16 14:56:59 +0600

    "fourth commit"

--- app/models/SalesOrderModel.php
+++ app/models/SalesOrderModel.php
@@ -270,10 +270,11 @@ class SalesOrderModel extends \DB\Cortex {
     public function getDataByBranch($data, $id): array {
         $from = $data['from'];
         $to = $data['to'];
-        $result = $this->db->exec("select sum(amount_unit) as total_quantity, sum(buying_price*amount_unit) 
-    as total_cost, sum(selling_price*amount_unit) as total_revenue 
-    from sales_product join sales_order on sales_product.sales_order_id=sales_order.id where 
-        date(sold_at) between '" . $from . "' and '" . $to . "' and sales_order.branch_id=$id");
+        $result = $this->db->exec("select branch.name as name, sum(amount_unit) as total_quantity, sum(buying_price*amount_unit) 
+        as total_cost, sum(selling_price*amount_unit) as total_revenue 
+        from sales_product join sales_order on sales_product.sales_order_id=sales_order.id join branch on branch.id=sales_order.branch_id
+        where date(sold_at) between '" . $from . "' and '" . $to . "' and sales_order.branch_id=$id");
+        $data['branch_name'] = $result[0]['name'];
         $data['revenue']['total_number_of_products_sold'] = intval($result[0]['total_quantity']);
         $data['revenue']['total_cost'] = intval($result[0]['total_cost']);
         $data['revenue']['net_revenue'] = intval($result[0]['total_revenue']);
